87ec9907bc6654d40e3978d8eeb7c1b15f55a329
mohammad <mohammadparvesbd02@gmail.com>
2025-01-25 12:51:32 +0300

    sadiya's portfolio

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+
+function opentab (tabname){
+    for (const tablink of tablinks) {
+        tablink.classList.remove("active-link")
+    }
+
+    for (const tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab")
+    }
+    event.target.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
