64d46c1b3bbed25f8d5f835d68ba5b89b0b684d1
mohammad <mohammadparvesbd02@gmail.com>
2025-01-26 18:04:51 +0300

    final submission

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 
-function opentab (tabname){
+function opentab(tabname) {
     for (const tablink of tablinks) {
         tablink.classList.remove("active-link")
     }
@@ -13,3 +13,32 @@ function opentab (tabname){
     event.target.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+
+var sidemenu = document.getElementById("side-menu");
+
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-210px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyyWCfaj2AYtmVgIlrKKdpiHfB2OQfKBbxIjWExdW8kwqBlc-PWEHiXnLo-uHCvsuMj8A/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = 'Message submitted successfully'
+            setTimeout(function(){
+                msg.innerHTML = ''
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
